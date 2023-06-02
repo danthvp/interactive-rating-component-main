11b68c64d525081670981a982ced13f2da93ef97
Daniel Hernández <dant.hdez02@gmail.com>
2023-06-02 14:32:24 -0600

    Submit interaction finished

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-let submit = document.getElementById("submit");
+let form = document.getElementById("form");
 let divContainer = document.getElementById('numberLstCont');
-let numberBtns = divContainer.getElementsByClassName('button')
+let numberBtns = divContainer.getElementsByClassName('button');
+let rateMsg = document.getElementById('rating-msg');
+let rateCard = document.getElementById('card-container');
+let cardSubmited = document.querySelector('.card-submitted');
+let current = document.getElementsByClassName('active');
 
 for(let i = 0; i < numberBtns.length; i++) {
     numberBtns[i].addEventListener('click', function() {
-    let current = document.getElementsByClassName('active');
         if(current.length > 0){
             current[0].className = current[0].className.replace(' active', '')
+            console.log(current[0])
         }
         this.className += ' active';
     })
 }
+
+
+
+form.addEventListener('submit', (e)=> {
+    rateMsg.innerHTML = `You selected ${current[0].value} out of 5`;
+    rateCard.style.display = 'none'
+    cardSubmited.style.display = 'flex';
+
+})
 
 
 // let selected = 0;
